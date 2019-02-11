8549924af8ab0a12d76a0230461f631a3b16a068
Oliver <oliver@circles.asia>
2019-02-11 12:20:12 +0800

    Added message and message id to the container object

--- containerClass.js
+++ containerClass.js
@@ -2,17 +2,21 @@ class Container {
     
     /**
      * The class constructor
-     * @param {Number} id 
-     * @param {String} name 
+     * @param {number} id 
+     * @param {string} name 
+     * @param {number} msgId
+     * @param {string} msg
      */
-    constructor(id, name) {
+    constructor(id, name, msgId, msg) {
         this.id = id;
         this.name = name;
+        this.msgId = msgId;
+        this.msg = msg;
     }
 
     /**
      * Get the id
-     * @returns {Number}
+     * @returns {number}
      */
     getId() {
         return this.id;
@@ -20,10 +24,26 @@ class Container {
 
     /**
      * Get the name
-     * @returns {String}
+     * @returns {string}
      */
     getName() {
         return this.name;
+    }
+
+    /**
+     * Get the message id
+     * @returns {number}
+     */
+    getMsgId() {
+        return this.msgId;
+    }
+
+    /**
+     * Get the message
+     * @returns {string}
+     */
+    getMsg() {
+        return this.msg;
     }
 
 } //end class
